9eb85e6854ee45d87d51457281c1450c784ccbaf
Adam <adam99martyniak@gmail.com>
2022-03-27 14:13:51 +0200

    Create basic architecture

--- LibraryAPI/Data/AppDbContext.cs
+++ LibraryAPI/Data/AppDbContext.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using LibraryAPI.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace LibraryAPI.Data
 {
-    public class AppDbContext
+    public class AppDbContext : IdentityDbContext<User>
     {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
     }
 }
--- LibraryAPI/Startup.cs
+++ LibraryAPI/Startup.cs
@@ -1,7 +1,9 @@
+using LibraryAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +34,8 @@ namespace LibraryAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "LibraryAPI", Version = "v1" });
             });
+
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
