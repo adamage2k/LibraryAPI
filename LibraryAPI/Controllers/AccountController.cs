10e6e9957c19a6e8a5495ccd372834ce936c6d12
Adam <adam99martyniak@gmail.com>
2022-03-27 14:35:35 +0200

    Add AccountController action

--- LibraryAPI/Controllers/AccountController.cs
+++ LibraryAPI/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LibraryAPI.DTOs;
+using LibraryAPI.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,5 +14,32 @@ namespace LibraryAPI.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("Login")]
+        public async Task<User> Login(LoginDTO loginDTO) 
+        {
+            var user = await _userManager.FindByNameAsync(loginDTO.Username);
+            if (user == null) 
+            {
+                throw new ArgumentException("User not exsists");
+            }
+            var result = await _signInManager.PasswordSignInAsync(loginDTO.Username, loginDTO.Passwd, false, false);
+
+            if (!result.Succeeded) 
+            {
+                throw new ArgumentException("Loggin Error");
+            }
+
+            return user;
+        }
+
     }
+
 }
---
+++ LibraryAPI/DTOs/LoginDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryAPI.DTOs
+{
+    public class LoginDTO
+    {
+        public string Username { get; set; }
+        public string Passwd { get; set; }
+    }
+}
